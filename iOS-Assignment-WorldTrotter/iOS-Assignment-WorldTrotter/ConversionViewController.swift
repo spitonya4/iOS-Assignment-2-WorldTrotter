e472f726d1b130e7cb627656aee756c9a28ca3d6
spitonya4 <spitonya@uvm.edu>
2021-11-16 11:18:48 -0500

    Did chapter 4

---
+++ iOS-Assignment-WorldTrotter/iOS-Assignment-WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Assignment-WorldTrotter
+//
+//  Created by Sam Pitonyak on 11/16/21.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+    }
+
+}
+
---
+++ iOS-Assignment-WorldTrotter/iOS-Assignment-WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  iOS-Assignment-WorldTrotter
+//
+//  Created by Sam Pitonyak on 11/16/21.
+//
+
+import UIKit
+
+class MapViewcontroller: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
