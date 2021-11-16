004791d34f580cddb58fd8082d93a55451c4dcba
spitonya4 <spitonya@uvm.edu>
2021-11-16 10:25:25 -0500

    Starting out project

--- CS275 iOS Assignment #2/ViewController.swift
+++ CS275 iOS Assignment #2/ViewController.swift
@@ -11,7 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
     }
 
 
